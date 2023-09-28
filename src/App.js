011f705bf4e371cf2d1816b6d2442b12397426ad
lawneng <95595420+lawneng@users.noreply.github.com>
2023-09-28 22:21:29 +0800

    Changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css'
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-  { "src": "/img/helmet-1.png", matched: false },
-  { "src": "/img/potion-1.png", matched: false },
-  { "src": "/img/ring-1.png", matched: false },
-  { "src": "/img/scroll-1.png", matched: false },
-  { "src": "/img/shield-1.png", matched: false },
-  { "src": "/img/sword-1.png", matched: false },
+  { "src": "/memory-game/img/helmet-1.png", matched: false },
+  { "src": "/memory-game/img/potion-1.png", matched: false },
+  { "src": "/memory-game/img/ring-1.png", matched: false },
+  { "src": "/memory-game/img/scroll-1.png", matched: false },
+  { "src": "/memory-game/img/shield-1.png", matched: false },
+  { "src": "/memory-game/img/sword-1.png", matched: false },
 ]
 
 function App() {
